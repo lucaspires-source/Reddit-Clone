ec6a374b10cceabb9bd3644068b300f772e81607
lucaspires-source <pires.lucas94@gmail.com>
2022-07-06 16:17:14 -0300

    more code cleaning

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Reddit Clone by Lucas Ribeiro Pires</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
