e1789a1e3d90e5156039f26364962a34bd6f1e29
lucaspires-source <pires.lucas94@gmail.com>
2022-07-07 12:41:09 -0300

    adding more icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,58 @@
 import Image from "next/image";
 import React from "react";
-import {ChevronDownIcon, HomeIcon, SearchIcon} from '@heroicons/react/solid'
-import {StarIcon} from '@heroicons/react/outline'
+import {
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+  MenuIcon,
+} from "@heroicons/react/solid";
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0">
         <Image
-            objectFit="contain"
+          objectFit="contain"
           src="https://logosmarcas.net/wp-content/uploads/2020/11/Reddit-Logo.png"
           layout="fill"
         />
       </div>
       <div className="flex items-center mx-7 xl:min-w-[300px] ">
-        <HomeIcon  className=" h-5 w-5"/>
+        <HomeIcon className=" h-5 w-5" />
         <p className="flex-1 ml-2 hidden lg:inline">Home</p>
-        <ChevronDownIcon className=" h-5 w-5"/>
+        <ChevronDownIcon className=" h-5 w-5" />
       </div>
       <form className="flex flex-1  items-center space-x-2 border border-gray-200 rounded-sm  bg-gray-100  px-3 py-1">
-        <SearchIcon className="h-6 w-6 text-gray-400"/>
-        <input type='text' placeholder="Search Reddit " className=" bg-transparent flex-1 outline-none" />
-        <button hidden  type="submit"/>
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search Reddit "
+          className=" bg-transparent flex-1 outline-none"
+        />
+        <button hidden type="submit" />
       </form>
+      <div className="flex text-gray-500 space-x-2 mx-5 hidden lg:inline-flex">
+            <SparklesIcon  className="icon"/>
+            <GlobeIcon  className="icon"/>
+            <VideoCameraIcon  className="icon"/>
+            <hr className="h-10 border border-gray-100"/>
+            <ChatIcon  className="icon"/>
+            <BellIcon  className="icon"/>
+            <PlusIcon  className="icon"/>
+            <SpeakerphoneIcon className="icon" />
+      </div>
+      <div className="ml-5 flex items-center  lg:hidden">
+        <MenuIcon  className="icon"/>
+      </div>
     </div>
   );
 }
