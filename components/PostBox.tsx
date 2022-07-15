81616ae05366673ff3db812c46497f8d2ec913a7
lucaspires-source <pires.lucas94@gmail.com>
2022-07-15 08:36:11 -0300

    adding icon to PostBox

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,13 +1,14 @@
 import { useSession } from "next-auth/react";
 import React from "react";
 import Avatar from "./Avatar";
-
+import { LinkIcon, PhotographIcon } from "@heroicons/react/outline";
 function PostBox() {
   const { data: session } = useSession();
   return (
-    <form>
+    <form className="sticky top-16 z-50 bg-white border border-gray-300 rounded-md p-2">
       <div className="flex items-center space-x-3 mr-2">
         <Avatar />
+
         <input
           disabled={!session}
           className=" flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
@@ -16,6 +17,8 @@ function PostBox() {
             session ? "Create a post by entering a title!" : "sign in to post"
           }
         />
+        <PhotographIcon className={`icon h-6 cursor-pointer text-gray-500`} />
+        <LinkIcon  className="icon h-6 text-gray-500"/>
       </div>
     </form>
   );
