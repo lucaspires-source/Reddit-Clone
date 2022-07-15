aabdd8c2cf9b36670c6a7004bfe94075499f7404
lucaspires-source <pires.lucas94@gmail.com>
2022-07-15 10:34:55 -0300

    finalizing postBox Component

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -9,7 +9,7 @@ type Props = {
 function Avatar({seed, large}: Props) {
   const { data: session } = useSession();
   return (
-    <div className={` overflow-hidden first:ml-2 relative h-10 w-10 rounded-full border-gray-300 bg-white ${large && 'h-20 w-20 '}`}>
+    <div className={` overflow-hidden relative h-10 w-10 rounded-full border-gray-300 bg-white ${large && 'h-20 w-20 '}`}>
       <Image
         layout="fill"
         src={`https://avatars.dicebear.com/api/open-peeps/${
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='max-w-sxl my-7 mx-auto'>
       <Head>
         <title>Reddit Clone by Lucas Ribeiro Pires</title>
         <link rel="icon" href="/favicon.ico" />
